92de6d4e22b895dcf4998507b1a09f43269a33e6
rafsof22-lgtm <rafsof22@gmail.com>
2026-07-19 12:11:31 +1000

    Export cost governor interfaces

--- jarvis_model_router/__init__.py
+++ jarvis_model_router/__init__.py
@@ -1,6 +1,33 @@
-"""Jarvis free-first model-router runtime scaffold."""
+"""Jarvis free-first model-router and cost-governor runtime."""
 
 from .config import ModelRoute, RouterConfig
 from .router import RouteDecision, route_task
+from .cost_governor import (
+    BudgetPolicy,
+    RouteQuote,
+    UsageEstimate,
+    cache_key,
+    estimate_cost,
+    load_provider_registry,
+    make_batches,
+    quote_routes,
+    run_measured_pilot,
+    select_route,
+)
 
-__all__ = ["ModelRoute", "RouteDecision", "RouterConfig", "route_task"]
+__all__ = [
+    "BudgetPolicy",
+    "ModelRoute",
+    "RouteDecision",
+    "RouteQuote",
+    "RouterConfig",
+    "UsageEstimate",
+    "cache_key",
+    "estimate_cost",
+    "load_provider_registry",
+    "make_batches",
+    "quote_routes",
+    "route_task",
+    "run_measured_pilot",
+    "select_route",
+]
