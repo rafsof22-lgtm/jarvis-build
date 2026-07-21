0bdfdc4e88fdf3526e91bd2b9e7465287531daf3
rafsof22-lgtm <rafsof22@gmail.com>
2026-07-22 09:45:09 +1000

    Export Cost profile schemas v17

--- src/jarvis_business/__init__.py
+++ src/jarvis_business/__init__.py
@@ -20,22 +20,21 @@ from .cost_plus_contracts_v2 import (
     WORKFLOW_CONTRACTS,
     architecture_decision,
 )
+from .cost_plus_profiles_v3 import (
+    CostProfileStore,
+    CustomerProfile,
+    WorkshopProfile,
+    VehicleProfile,
+    SupplierProfile,
+    OrderProfile,
+    run_synthetic_order_to_billing,
+)
 
 __all__ = [
-    "BUSINESS_COST_PLUS_ONE",
-    "BUSINESS_JENOK",
-    "CatalogueStore",
-    "CostInput",
-    "DeploymentController",
-    "OrderVerifier",
-    "PricingEngine",
-    "ProcurementService",
-    "SourceReconstructor",
-    "BillingPolicyRegistry",
-    "CostPlusContractEngine",
-    "EventEnvelope",
-    "LockerAllocator",
-    "SQLiteEventLedger",
-    "WORKFLOW_CONTRACTS",
-    "architecture_decision",
+    "BUSINESS_COST_PLUS_ONE", "BUSINESS_JENOK", "CatalogueStore", "CostInput",
+    "DeploymentController", "OrderVerifier", "PricingEngine", "ProcurementService",
+    "SourceReconstructor", "BillingPolicyRegistry", "CostPlusContractEngine",
+    "EventEnvelope", "LockerAllocator", "SQLiteEventLedger", "WORKFLOW_CONTRACTS",
+    "architecture_decision", "CostProfileStore", "CustomerProfile", "WorkshopProfile",
+    "VehicleProfile", "SupplierProfile", "OrderProfile", "run_synthetic_order_to_billing",
 ]
