f8ded213c65fe3d36d3fc374b405f38f08641f7c
rafsof22-lgtm <rafsof22@gmail.com>
2026-07-21 20:01:47 +1000

    Export read-only database inspector

--- src/jarvis_integrations/__init__.py
+++ src/jarvis_integrations/__init__.py
@@ -1,8 +1,12 @@
-from .n8n_adapter_v1 import (
-    N8nAdapter,
-    N8nExecutionPolicy,
-    N8nRequest,
-    ReplayStore,
-)
+from .n8n_adapter_v1 import N8nAdapter, N8nExecutionPolicy, N8nRequest, ReplayStore
+from .read_only_db_inspector_v1 import InspectorPolicy, QuerySpec, ReadOnlyDatabaseInspector
 
-__all__ = ["N8nAdapter", "N8nExecutionPolicy", "N8nRequest", "ReplayStore"]
+__all__ = [
+    "N8nAdapter",
+    "N8nExecutionPolicy",
+    "N8nRequest",
+    "ReplayStore",
+    "InspectorPolicy",
+    "QuerySpec",
+    "ReadOnlyDatabaseInspector",
+]
