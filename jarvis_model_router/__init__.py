2ed87ae669309cc78e8476b0d373e2caae6bb407
rafsof22-lgtm <rafsof22@gmail.com>
2026-07-21 18:05:03 +1000

    Export universal selector runtime

--- jarvis_model_router/__init__.py
+++ jarvis_model_router/__init__.py
@@ -1,46 +1,24 @@
 """Jarvis free-first model-router and cost-governor runtime."""
-
 from .config import ModelRoute, ROUTING_MODES, RouterConfig
 from .router import PanelDecision, RouteDecision, route_panel, route_task
-from .local_models import (
-    HostCapacity,
-    LocalModelDecision,
-    LocalModelProfile,
-    select_local_model,
+from .selector import (
+    ConnectedModel, ModelCatalogue, ModelResponseRecord, ParallelPanelPlan,
+    SHARED_SELECTOR_SURFACES, build_model_catalogue, build_synthesis_packet,
+    plan_parallel_panel, selector_surface_contract, verify_consolidated_response,
 )
+from .local_models import HostCapacity, LocalModelDecision, LocalModelProfile, select_local_model
 from .cost_governor import (
-    BudgetPolicy,
-    RouteQuote,
-    UsageEstimate,
-    cache_key,
-    estimate_cost,
-    load_provider_registry,
-    make_batches,
-    quote_routes,
-    run_measured_pilot,
+    BudgetPolicy, RouteQuote, UsageEstimate, cache_key, estimate_cost,
+    load_provider_registry, make_batches, quote_routes, run_measured_pilot,
     select_route,
 )
-
 __all__ = [
-    "BudgetPolicy",
-    "HostCapacity",
-    "LocalModelDecision",
-    "LocalModelProfile",
-    "ModelRoute",
-    "PanelDecision",
-    "ROUTING_MODES",
-    "RouteDecision",
-    "RouteQuote",
-    "RouterConfig",
-    "UsageEstimate",
-    "cache_key",
-    "estimate_cost",
-    "load_provider_registry",
-    "make_batches",
-    "quote_routes",
-    "route_panel",
-    "route_task",
-    "run_measured_pilot",
-    "select_local_model",
-    "select_route",
+    "BudgetPolicy", "ConnectedModel", "HostCapacity", "LocalModelDecision",
+    "LocalModelProfile", "ModelCatalogue", "ModelResponseRecord", "ModelRoute",
+    "PanelDecision", "ParallelPanelPlan", "ROUTING_MODES", "RouteDecision",
+    "RouteQuote", "RouterConfig", "SHARED_SELECTOR_SURFACES", "UsageEstimate",
+    "build_model_catalogue", "build_synthesis_packet", "cache_key", "estimate_cost",
+    "load_provider_registry", "make_batches", "plan_parallel_panel", "quote_routes",
+    "route_panel", "route_task", "run_measured_pilot", "select_local_model",
+    "select_route", "selector_surface_contract", "verify_consolidated_response",
 ]
