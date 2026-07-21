38223bdc160d0af1cc3eb675f90f40160468f58d
rafsof22-lgtm <rafsof22@gmail.com>
2026-07-21 19:55:13 +1000

    Add Jarvis integrations package

---
+++ src/jarvis_integrations/__init__.py
@@ -0,0 +1,8 @@
+from .n8n_adapter_v1 import (
+    N8nAdapter,
+    N8nExecutionPolicy,
+    N8nRequest,
+    ReplayStore,
+)
+
+__all__ = ["N8nAdapter", "N8nExecutionPolicy", "N8nRequest", "ReplayStore"]
