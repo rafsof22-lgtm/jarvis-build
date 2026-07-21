943e2ec0966ddc6684180255d29004fa038712ca
rafsof22-lgtm <rafsof22@gmail.com>
2026-07-21 18:31:17 +1000

    Expose integrated Command Centre v1.3

--- jarvis_command_centre/__init__.py
+++ jarvis_command_centre/__init__.py
@@ -1,3 +1,6 @@
 """Jarvis command-centre federation package."""
 
-__version__ = "1.0.0"
+from .integrated_v13 import build_model_control_snapshot, build_snapshot, render_html
+
+__version__ = "1.3.0"
+__all__ = ["build_model_control_snapshot", "build_snapshot", "render_html"]
