9057506d7ff49dcf40f309f941a4e9be98475f03
rafsof22-lgtm <rafsof22@gmail.com>
2026-07-22 09:44:28 +1000

    Export Command Centre v1.4 full-stack surface

--- jarvis_command_centre/__init__.py
+++ jarvis_command_centre/__init__.py
@@ -1,6 +1,7 @@
 """Jarvis command-centre federation package."""
 
-from .integrated_v13 import build_model_control_snapshot, build_snapshot, render_html
+from .full_stack_v17 import build_full_stack_surface, build_snapshot, render_html
+from .integrated_v13 import build_model_control_snapshot
 
-__version__ = "1.3.0"
-__all__ = ["build_model_control_snapshot", "build_snapshot", "render_html"]
+__version__ = "1.4.0"
+__all__ = ["build_full_stack_surface", "build_model_control_snapshot", "build_snapshot", "render_html"]
